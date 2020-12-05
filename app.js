18f3cc5b2dfe5bd85db22aff13c55a99f2480b57
ashu goyal <ashugoyal6799@gmail.com>
2020-12-05 11:51:18 +0530

    newlap updated

--- app.js
+++ app.js
@@ -1,76 +1,77 @@
 // .env file is generally used to keep your secret keys and api so that it can't see by anyone
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const ejs  = require('ejs');
-const bcrypt = require('bcrypt');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const bcrypt = require("bcrypt");
 
+// in new laptop
 
-const app=express();
+const app = express();
 
-app.use(express.static('public'));
-app.set('view engine','ejs');
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true , useUnifiedTopology: true});
+mongoose.connect("mongodb://localhost:27017/userDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 // user defined schema for using encryption
 const userSchema = new mongoose.Schema({
-    email:String,
-    password : String
+  email: String,
+  password: String,
 });
 
+const User = new mongoose.model("User", userSchema);
 
-const User  = new mongoose.model("User",userSchema);
-
-app.get('/',function(req,res){
-    res.render('home');
+app.get("/", function (req, res) {
+  res.render("home");
 });
-app.get('/register',function(req,res){
-    res.render('register');
+app.get("/register", function (req, res) {
+  res.render("register");
 });
-app.get('/login',function(req,res){
-    res.render('login');
+app.get("/login", function (req, res) {
+  res.render("login");
 });
 
-app.post('/register',function(req,res){
-    bcrypt.hash(req.body.password, 5, function(err, hash) {
-        const newUser = new User({
-            email : req.body.username,
-            password : hash
-        });
-        newUser.save(function(err){
-            if(err){
-                console.log(err);
-            }else{
-                res.render("secrets");
-            }
-        });
+app.post("/register", function (req, res) {
+  bcrypt.hash(req.body.password, 5, function (err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash,
     });
+    newUser.save(function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
+  });
 });
 
-app.post('/login',function(req,res){
-    const username = req.body.username;
-    const password = req.body.password;
-    
-    User.findOne({email:username},function(err,foundUser){
-        if(err){
-            console.log(err);
-        }
-        else{
-            if(foundUser){
-                bcrypt.compare(password, foundUser.password, function(err, result) {
-                    if(result==true){
-                        res.render("secrets");
-                    }
-                });
-            }
-        }
-    })
+app.post("/login", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({ email: username }, function (err, foundUser) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        bcrypt.compare(password, foundUser.password, function (err, result) {
+          if (result == true) {
+            res.render("secrets");
+          }
+        });
+      }
+    }
+  });
 });
 
-
-app.listen(3000,function(req,res){
-    console.log("server is listening at port 3000");
+app.listen(3000, function (req, res) {
+  console.log("server is listening at port 3000");
 });
